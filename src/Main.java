94bcdc6aca799f53212a3775008799aff65236c3
Viktorryy <viktory_o@rambler.ru>
2022-10-11 14:47:29 +0400

    решение

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        int[] arrayStraight = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] arrayReverse = new int[arrayStraight.length];
+        int indexReverse;
+
+        for (int i = arrayStraight.length-1 ; i >=0 ; i--) {
+
+            indexReverse = arrayStraight.length - 1 - i;
+            arrayReverse[indexReverse] = arrayStraight[i];
+
+        }
+
+        for (int i = 0; i < arrayStraight.length; i++) {
+            System.out.print("arrayStraight[" + i + "] = " + arrayStraight[i]);
+            System.out.println("  arrayReverse["+i+"] = " + arrayReverse[i]);
+        }
+
+
     }
 }
